8f4044ce8cb8e42eb80561561810bf6ef17ffa19
tronje <hi@tron.je>
2017-07-12 21:25:24 +0200

    minor changes

--- xorg/.screenlayout/default.sh
+++ xorg/.screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x272 --rotate normal --output DP-5 --off --output DP-4 --off --output DVI-I-1 --off --output DP-2 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-0 --off
+xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x360 --rotate normal --output DP-5 --off --output DP-4 --mode 2560x1440 --pos 1920x0 --rotate normal --output DVI-I-1 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
