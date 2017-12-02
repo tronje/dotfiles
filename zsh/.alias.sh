7b4f55d2558af05d54d9856003f18efb48299cd8
Tronje Krabbe <hi@tron.je>
2017-12-02 12:54:39 +0100

    Add some nifty aliases

--- zsh/.alias.sh
+++ zsh/.alias.sh
@@ -10,6 +10,8 @@ alias sc='sudo systemctl'
 alias scu='systemctl --user'
 alias wifi='sudo systemctl restart netctl-auto@wlp4s0'
 alias nspawn='sudo systemd-nspawn'
+alias mctl='machinectl'
+alias smctl='sudo machinectl'
 
 # pacman
 alias pac='pacman'
@@ -18,11 +20,17 @@ alias spac='sudo pacman'
 # neovim
 alias vi='nvim'
 alias vim='nvim'
+alias gvi='nvim $(git status --porcelain | cut -s -f 2 -d M)'
+alias svi='sudo -E nvim'
+alias svim='sudo -E nvim'
 
 # git aliases
+_git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
+_git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
 alias g='git'
 alias gls='git status'
 alias gl='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
+alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
 
 # cargo
 alias clippy='rustup run nightly cargo clippy'
