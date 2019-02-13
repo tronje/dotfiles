39c624564b1654625be98520b76c283ff81b81fd
Tronje Krabbe <hi@tron.je>
2019-02-13 15:51:18 +0100

    i3: Use different blur method in lock script
    Instead of actually blurring, we just do two resizes.

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -10,8 +10,9 @@ if [ $1 = "blur" ]; then
     img=/tmp/lock.png
     lock=/home/tronje/.config/i3/lock_resized.png
     scrot $img
-    # convert $img -blur 0x3 $img
-    convert $img -filter Gaussian -resize 50% -define filter:sigma=2 -resize 200% $img
+    convert $img -resize 480x270 $img
+    convert $img -resize 2560x1440 $img
+    # convert $img -filter Gaussian -resize 50% -define filter:sigma=2 -resize 200% $img
     if [ $(xrandr | rg -c "\bconnected\b") -ge 2 ]; then
         composite -geometry +0+400 -gravity North $lock $img $img
     else
