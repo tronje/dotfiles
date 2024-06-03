15104deb92052b21c199fa4f1576c086eb3c2b72
Tronje Krabbe <tkrabbe@jusst.de>
2024-06-03 14:37:34 +0200

    nvim: Set relativenumber in addition to number
    This causes the line under the cursor to show the absolute line number,
    and all other lines to show a relative line number.

--- nvim/.config/nvim/lua/basics.lua
+++ nvim/.config/nvim/lua/basics.lua
@@ -33,6 +33,7 @@ vim.opt.listchars = { tab = '→·' }
 vim.opt.matchpairs:append('<:>')
 vim.opt.mouse = 'a'
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.ruler = true
 vim.opt.scrolloff = 5
 vim.opt.shiftwidth = 4
